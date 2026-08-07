3efbdff9b0295bd5f48646e20fefef62b971ef9e
Aditya Kumar <adityakumar20741@gmail.com>
2026-08-07 20:31:59 +0530

    minor twerk

--- Client/src/Components/EmployeeTable.jsx
+++ Client/src/Components/EmployeeTable.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import api from "../Utils/api";
+import { useState } from "react";
 
-export function EmployeeTable({ employees,fetchEmployees }) {
+export function EmployeeTable({ employees, fetchEmployees }) {
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
     const navigate = useNavigate();
 
     const handleEdit = (id) => {
         navigate(`/employees/${id}`);
     };
 
-    const handleDelete = async(id) => {
+    const handleDelete = async () => {
         try {
-            await api.delete(`/employees/${id}`);
+            await api.delete(`/employees/${selectedEmployee._id}`);
             toast.success("Employee deleted successfully!");
             await fetchEmployees();
+            document.getElementById("delete_modal").close();
             navigate("/", { replace: true });
         } catch (error) {
             console.log(error);
@@ -26,36 +29,65 @@ export function EmployeeTable({ employees,fetchEmployees }) {
     }
 
     return (
-        <div className="overflow-x-auto">
-            <table className="table text-center w-full">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Department</th>
-                        <th colSpan={2}>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map((employee) => (
-                        <tr key={employee._id}>
-                            <td>{employee.name}</td>
-                            <td>{employee.position}</td>
-                            <td>{employee.department}</td>
-                            <td>
-                                <button className="btn btn-soft btn-secondary btn-sm mr-5" onClick={() => handleEdit(employee._id)}>
-                                    Edit
-                                </button>
-                            </td>
-                            <td>
-                                <button className="btn btn-soft btn-error btn-sm" onClick={() => handleDelete(employee._id)}>
-                                    Delete
-                                </button>
-                            </td>
+        <>
+            <dialog id="delete_modal" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Delete Employee</h3>
+
+                    <p className="py-4">
+                        Are you sure you want to delete{""} {selectedEmployee?.name}?
+                    </p>
+
+                    <div className="modal-action">
+                        <button
+                            className="btn btn-error"
+                            onClick={handleDelete}
+                        >
+                            Delete
+                        </button>
+
+                        <form method="dialog">
+                            <button className="btn">Cancel</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
+
+            <div className="overflow-x-auto">
+                <table className="table text-center w-full">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Department</th>
+                            <th colSpan={2}>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {employees.map((employee) => (
+                            <tr key={employee._id}>
+                                <td>{employee.name}</td>
+                                <td>{employee.position}</td>
+                                <td>{employee.department}</td>
+                                <td>
+                                    <button className="btn btn-soft btn-secondary btn-sm mr-5" onClick={() => handleEdit(employee._id)}>
+                                        Edit
+                                    </button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-soft btn-error btn-sm" onClick={() => {
+                                        setSelectedEmployee(employee);
+                                        document.getElementById("delete_modal").showModal();
+                                    }}>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
